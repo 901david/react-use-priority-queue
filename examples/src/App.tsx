e5753259c2e52cbe7ffcfae38cf0466cb344bc27
901david <vdavidhammond@gmail.com>
2020-02-29 10:01:12 -0700

    broke out examples to separate components

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { usePriorityQueue } from "react-use-priority-queue";
 
-const Wrapper = styled.div`
+import { ExampleOne } from "./ExampleOne";
+
+export const Wrapper = styled.div`
   table {
     font-family: arial, sans-serif;
     border-collapse: collapse;
@@ -20,60 +22,11 @@ const Wrapper = styled.div`
     background-color: #dddddd;
   }
 `;
-interface IInitialData {}
 
-const InitialData = [
-  { data: "Take out trash", priority: 1 },
-  { data: "Walk the dog", priority: 4 }
-];
-
-const App = () => {
-  const [{ priorityQueue, lastDequeuedItem }, add, remove] = usePriorityQueue(
-    "MIN",
-    InitialData
-  );
-
-  const enqueueNewData = () => {
-    add("Walk dog around block", Math.floor(Math.random() * 25));
-  };
-
-  const dequeueData = () => {
-    const val = remove();
-    console.log(val);
-  };
-
+const App: React.FC<{}> = () => {
   return (
     <Wrapper>
-      <button onClick={enqueueNewData}>Enqueue Another Item</button>
-      <button onClick={dequeueData}>Dequeue Another Item</button>
-      <table>
-        <tr>
-          <th>TODO</th>
-          <th>Priority</th>
-        </tr>
-        {priorityQueue.length > 0 &&
-          priorityQueue.map((todo: any, idx: number) => (
-            <tr key={idx}>
-              <td>{todo.data}</td>
-              <td>{todo.priority}</td>
-            </tr>
-          ))}
-      </table>
-      <hr />
-      <h1> Last Item removed</h1>
-      {lastDequeuedItem === undefined && <h4>Nothing has been dequeued</h4>}
-      {lastDequeuedItem !== undefined && (
-        <table>
-          <tr>
-            <th>TODO</th>
-            <th>Priority</th>
-          </tr>
-          <tr>
-            <td>{lastDequeuedItem.data}</td>
-            <td>{lastDequeuedItem.priority}</td>
-          </tr>
-        </table>
-      )}
+      <ExampleOne />
     </Wrapper>
   );
 };
---
+++ examples/src/ExampleOne.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { usePriorityQueue } from "react-use-priority-queue";
+
+import { Wrapper } from "./App";
+
+const InitialData = [
+  { data: "Take out trash", priority: 1 },
+  { data: "Walk the dog", priority: 4 }
+];
+
+export const ExampleOne = () => {
+  const [{ priorityQueue, lastDequeuedItem }, add, remove] = usePriorityQueue(
+    "MIN",
+    InitialData
+  );
+
+  const enqueueNewData = () => {
+    add("Walk dog around block", Math.floor(Math.random() * 25));
+  };
+
+  const dequeueData = () => {
+    const val = remove();
+    console.log(val);
+  };
+
+  return (
+    <Wrapper>
+      <h1>Example 1 - using sorted array with Min Priority Queue</h1>
+      <button onClick={enqueueNewData}>Enqueue Another Item</button>
+      <button onClick={dequeueData}>Dequeue Another Item</button>
+      <table>
+        <tr>
+          <th>TODO</th>
+          <th>Priority</th>
+        </tr>
+        {priorityQueue.length > 0 &&
+          priorityQueue.map((todo: any, idx: number) => (
+            <tr key={idx}>
+              <td>{todo.data}</td>
+              <td>{todo.priority}</td>
+            </tr>
+          ))}
+      </table>
+      <hr />
+      <h1> Last Item removed</h1>
+      {lastDequeuedItem === undefined && <h4>Nothing has been dequeued</h4>}
+      {lastDequeuedItem !== undefined && (
+        <table>
+          <tr>
+            <th>TODO</th>
+            <th>Priority</th>
+          </tr>
+          <tr>
+            <td>{lastDequeuedItem.data}</td>
+            <td>{lastDequeuedItem.priority}</td>
+          </tr>
+        </table>
+      )}
+    </Wrapper>
+  );
+};
